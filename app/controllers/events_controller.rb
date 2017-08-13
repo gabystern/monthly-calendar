5b976de7d1aa73b23b060ff2e1099ce6edf2f44c
gabystern <stern.gabrielle@gmail.com>
2017-08-13 11:41:08 -0400

    created table with monthly values. able to create a new event

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,3 +1,45 @@
+require 'pry'
+
 class EventsController < ApplicationController
+
+  def index
+    @events = Event.all
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new()
+  end
+
+  def create
+    @event = Event.new(event_params)
+    find_event = Event.find_by(time: @event.time)
+    redirect_to events_path
+    if !find_event.present?
+      @event.save
+      redirect_to events_path
+    else
+      flash[:notice] = "There is already an event at that time. Please try again."
+      return render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:name, :time)
+  end
 
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Event.create(name: "first event", time: '2017-08-12 17:42:33 +0000')
+Event.create(name: "second event", time: '2017-08-12 18:42:33 +0000')
